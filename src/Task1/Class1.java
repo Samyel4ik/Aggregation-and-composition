4f797bc40eb5cff8b4809b390920e297f635d9e7
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-03-22 21:51:06 +0300

    T1

--- src/Task1/Class1.java
+++ src/Task1/Class1.java
@@ -8,9 +8,7 @@ public class Class1 {
         Word word = new Word("Hello");
         Word word2 = new Word("Java");
         Word word3 = new Word("Cat");
-
         Word[] words = new Word[]{word, word2, word3};
-
         Word[] words1 = new Word[]{
                 new Word("Customer"),
                 new Word("buys"),
@@ -25,14 +23,28 @@ public class Class1 {
         Sentence sentence1 = new Sentence(words1);
         Sentence[] sentences = new Sentence[]{heading, sentence, sentence1};
 
-        Text text = new Text(sentences);
-        System.out.println(text);
-
+        Text text = new Text(sentences); //исходный текст
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите предложение - ");
-        String str = scanner.nextLine();
+        System.out.println("Выберите пункт меню:" + "\n" +
+                "1.Дополнить текст." + "\n" +
+                "2.Распечатать текст." + "\n" +
+                "3.Распечатать закголовок текста.");
+        System.out.print("Ваш выбор: ");
+        int x = Integer.parseInt(scanner.nextLine());
+
+        if (x == 1) {
+            System.out.println("Введите предложение - ");
+            String str = scanner.nextLine();
+            Text text1 = addText(text,sentences,str);
+            System.out.println(text1);
+        }
+
+
+    }
+
+    public static Text addText(Text text, Sentence[] sentences, String str) {
 
         Text text1 = new Text(text.addSentence(sentences, str));
-        System.out.println(text1);
+        return text1;
     }
 }
